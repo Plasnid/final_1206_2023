6d755881c4786829fbb8c7ea30c475db1438aa55
Plasnid <stuart@sackler.ca>
2023-04-16 22:56:19 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+/**
+* * Section 1: Arrays
+ */
+
+/** 
+ * ! Q1a: Using Array Co-ordinates, and the array below create 5 words
+ * !  that contain at least 4 letters each.  Console log each word using template literals and array locations.
+ * 
+*/
+
+let letters  = [
+    ["a", "b", "c", "d", "e"],
+    ["f", "g", "h", "i", "j",],
+    ["k", "l", "m", "n", "o",],
+    ["p", "q", "r", "s", "t",],
+    ["u", "v", "w", "x", "y", "z"]
+]
+
+/**
+ * ! Q2a:  Take the sentence below, and using the split command, transform
+ * ! it into an array, then console log it in the opposite order.
+ * * eg. 
+ * ! great is cheese think I 
+ * *         SHOULD BECOME
+ *  ! I think cheese is great
+ */
+
+let sentence  = ".Code VS of glow warm the than world this in better nothing is There";
+
+
+/**
+ * * Section 2: Functions
+ */
+
+/**
+ * ! Q2a: Create a fat arrow function that creates a button tag on the screen.  
+ * !      Make the text of that button your name and your student # using javascript.
+ * !      Write a class in the css file that changes the background to a colour of your choice
+ * !      Use Javascript to add that class to the button
+ * !      Use Javascript(no libraries) to add a click event that console logs the same text as on the button
+ */
+
+/**
+ * ! Q2b: Create a function declaration that takes in 4 parameters. 
+ * !    -an html tag to create
+ * !    -text to go into the html tag
+ * !    -the style to go onto the html tag
+ * !    -a boolean to determine if the tag is console logged or printed to the screen
+ * !
+ * !    Create the styles in your css file.  Run the function 4 times.  Twice with console log output, 
+ * !    and twice with output added to the html through dom manipulation.
+ */
+
+/**
+ * * Section 3: Looping
+ */
+
+/**
+ * ! Q3a: Create an array of 5 objects.  In each object have two values.
+ * !        -the name of an animal
+ * !        -a descriptive word about an animal
+ * !      Use a for loop, with createElement and appendChild to add a list of sentences to the screen
+ * !      Each sentence should have the following format: "The __animalName__ is __animalProperty__"
+ */
+
+/**
+ * ! Q3b: Make an object describing a food.  Add 5 properties of that food use key/value pairs
+ * 
+ * !      Make an entry in your object that contains an array of ingredients.  Loop backwards through the array
+ * !      and console log each array entry individually.
+ */
